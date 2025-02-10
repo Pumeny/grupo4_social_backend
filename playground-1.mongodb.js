24886f98f897f790fa9d27f593ac8d24fd089b00
Pumeny Diaz <pumenydc@gmail.com>
2025-02-10 23:57:32 +0100

    cambios endpoint instal mongo

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,2 +1,129 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// Create a new database.
+use('social_group_DB');
+
+// Create a new collection.
+db.createCollection('zonas');
+db.createCollection('agricultores');
+
+db.zonas.insertMany([
+
+    {nombre:"Horta Nord", 
+    },
+    {nombre:"Horta Sur", 
+    },
+    {nombre:"Horta Est", 
+    },
+]);
+
+let zonasId = db.zonas.findOne({nombre: "Horta Nord"})._id;
+
+db.agricultores.insertMany([
+    {
+    //fotoPerfil: "url",
+    nombre:"Ana", 
+    apellido:"Rodríguez",
+    direccion:"calle matata, 3",
+    telefono:"+342838392",
+    email:"pepita@gmail.com",
+     //Espacio / Huerta
+    localizacion:"Horta Nord",
+    horario:"9am - 2pm",
+    descripcion:"Soy Ana y cultivo frutas y verduras frescas en la zona este. Utilizo prácticas agrícolas sostenibles y ofrezco mis productos en cajas, que incluyen una selección de frutas y hortalizas locales, perfectas para quienes aprecian los productos frescos y naturales. Soy super maja y vendo barato y de calidad",
+    //Espacio / Productos
+    tipoCaja:"Caja Grande",
+    disponibilidad:"Lunes",
+    precio:"30€",
+    envioRecogida:"Envío",
+    tipoPago:"Tarjeta",
+    zonaID: zonasId
+    },
+]);
+    zonasId = db.zonas.findOne({nombre: "Horta Est"})._id;
+
+    db.agricultores.insertMany([
+    {
+//fotoPerfil: "url",
+    nombre:"María", 
+     apellido:"Sanz",
+     direccion:"calle sol, 5",
+     telefono:"+342838392",
+     email:"juana@gmail.com",
+     //Espacio / Huerta
+     localizacion:"Horta Est",
+     horario:"8am - 1pm",
+     descripcion:"Soy María y me dedico al cultivo de frutas y verduras en la zona sur. Mis productos están disponibles en cajas, con una mezcla de lo mejor de la temporada. Cada caja está llena de frutas y verduras frescas, cultivadas con mucho cuidado para que puedas disfrutar de productos saludables y sabrosos. Me gusta cantar y bailar, comprame",
+    //Espacio / Productos
+    tipoCaja:"Caja Grande",
+    disponibilidad:"Martes",
+    precio:"35€",
+    envioRecogida:"Envío",
+    tipoPago:"Tarjeta",
+    zonaID: zonasId
+    },
+]);
+    zonasId = db.zonas.findOne({nombre: "Horta Sud"})._id;
+
+    db.agricultores.insertMany([
+
+    {
+        // fotoPerfil: "url",
+            nombre:"Pedro", 
+             apellido:"Miravalls",
+             direccion:"calle Luna, 7",
+             telefono:"+34284692",
+             email:"PedroyMaria@gmail.com",
+             //Espacio / Huerta
+             localizacion:"Horta Sud",
+             horario:"8am - 1pm",
+             descripcion:"Me llamo Pedro y cultivo frutas y verduras en la zona norte. Mi enfoque es ofrecer productos frescos y de calidad, y vendo mis cosechas en cajas llenas de productos de temporada, ideales para quienes buscan ingredientes frescos y locales",
+            //Espacio / Productos
+            tipoCaja:"Caja Grande",
+            disponibilidad:"Jueves",
+            precio:"25€",
+            envioRecogida:"Recogida",
+            tipoPago:"Efectivo",
+            zonaID: zonasId
+            },
+ ]);
+
+ let zonaBusqueda = "Horta Nord";//duda si solo se hace de una zona o de todas
+
+let zonaId = db.zonas.findOne({ nombre: zonaBusqueda })._id;
+
+let agricultores = db.agricultores.find({ zonaId: zonaId }).toArray();
 
 
+// The prototype form to create a collection:
+/* db.createCollection( <name>,
+  {
+    capped: <boolean>,
+    autoIndexId: <boolean>,
+    size: <number>,
+    max: <number>,
+    storageEngine: <document>,
+    validator: <document>,
+    validationLevel: <string>,
+    validationAction: <string>,
+    indexOptionDefaults: <document>,
+    viewOn: <string>,
+    pipeline: <pipeline>,
+    collation: <document>,
+    writeConcern: <document>,
+    timeseries: { // Added in MongoDB 5.0
+      timeField: <string>, // required for time series collections
+      metaField: <string>,
+      granularity: <string>,
+      bucketMaxSpanSeconds: <number>, // Added in MongoDB 6.3
+      bucketRoundingSeconds: <number>, // Added in MongoDB 6.3
+    },
+    expireAfterSeconds: <number>,
+    clusteredIndex: <document>, // Added in MongoDB 5.3
+  }
+)*/
+
+// More information on the `createCollection` command can be found at:
+// https://www.mongodb.com/docs/manual/reference/method/db.createCollection/
--- server.js
+++ server.js
@@ -2,45 +2,136 @@ import express from 'express';
 import morgan from 'morgan';
 import  { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
+import { MongoClient, ObjectId, ObjetId }  from 'mongodb';
+const dbName = 'social_group_DB';
 
 const app = express();
-
-let agricultores = [
-    { id: '1', name: 'Josefo', lastname: "Sanz", description: "Soy super majo y vendo barato y de calidad", direction: "calle matata, 3", phoneNumber: "+342838392", mail: "josefinsanz@gmail.com"},
-    { id: '2', name: 'María', lastname: "Gutierrez", description: "Me gusta cantar y bailar, comprame", direction: "calle sol, 5", phoneNumber: "+342844392", mail: "maría@gmail.com"}
-];
 
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors()); // Habilitar CORS
+
+const mongoUrl = 'mongodb://localhost:27017/';
+ let db; 
+
+ MongoClient.connect(mongoUrl, {useNewUrlParser: true, useUniFiedTopology: true})
+ .then(client => {
+    console.log('Conectado a MongoDB');
+    db = client.db(dbName);
+ }) 
+ .catch(err => {
+    console.error('Error al conectar MongoDB:', err);
+ });
+ 
 // API
 
 app.post('/agricultores', (req, res) => {
-    console.log("Solicitud recibida:", req.body);
     const agricultor = req.body;
-    agricultor.id = uuidv4(); 
-    agricultores.push(agricultor); 
-    res.json(agricultor); 
-});
+    agricultor.id = uuidv4();
+    agricultor.createdAt = new Date();
+    agricultor.updatedAt = new Date();
+    createAgricultor(agricultor);
+    res.json(agricultor);
+    });
+    //  let peticion = req.body;  try {
+          //Connect to the "insertDB" database and access its "haiku" collection
+          //const database = client.db("social_group_DB");
+          //const agricultores = database.collection("agricultores");
+          // Create a document to insert
+        /*const doc = {
+                fotoPerfil: "url",
+                nombre:peticion.nombre,
+                 apellido:peticion.apellido,
+                 direccion:peticion.direccion,
+                 telefono:peticion.telefono,
+                 email:peticion.email,
+                 //Espacio / Huerta
+                 localizacion:peticion.localizacion,
+                 horario:peticion.horarios,
+                 descripcion:peticion.descripcion,
+                //Espacio / Productos
+                tipoCaja:peticion.tipoCaja,
+                disponibilidad:peticion.disponibilidad,
+                precio:peticion.precio,
+                envioRecogida:peticion.envioRecogida,
+                tipoPago:peticion.tipoPago,
+                },
+                const result = await agricultores.insertOne(doc);
+                // console.log("Solicitud recibida:", req.body);
+    //const agricultor = req.body;
+    //agricultor.id = uuidv4(); 
+    //agricultores.push(agricultor); 
+    //res.status(201); 
+//});*/
 
 //get all agricultores
-app.get('/agricultores', (req, res) => {
-    res.json(agricultores);
+const getAgricultores = ('/agricultores') => {
+    return new Promise ((resolve, reject) => {
+        agricultoresColection.find().toArray((err, result) => {
+if (err) {
+    reject(err);
+} else {
+    resolve(result);
+}
+    });
 });
+}
 
 // Endpoint para obtener el detalle de un agricultor por ID
-app.get('/agricultores/:id', (req, res) => {
-    const agricultorId = req.params.id; // Obtener el ID de los parámetros de la URL
-
+function getAgricultor(/agricultor/:id) => {
+    return new Promise ((resolve, reject) => {
+        agricultoresColection.findOne ({_id: ObjectId(agricultorId)})
+        .then (agricultor => resolve(agricultor))
+        .catch (errorFind => reject(errorFind));
+    });
+}
+    //, //(req, res) => {
+    //const agricultorId = req.params.id; // Obtener el ID de los parámetros de la URL
     // Buscar el agricultor en la lista
-    const agricultor = agricultores.find((agricultor) => agricultor.id === agricultorId);
+    // const agricultor = agricultores.find((agricultor) => agricultor.id === agricultorId);
 
-    if (agricultor) {
-        res.json(agricultor); // Enviar los datos del agricultor encontrado
-    } else {
-        res.status(404).json({ error: "Agricultor no encontrado" }); // Enviar error si no existe
+    // if (agricultor) {
+        // res.json(agricultor); // Enviar los datos del agricultor encontrado
+    // } else {
+        // res.status(404).json({ error: "Agricultor no encontrado" }); // Enviar error si no existe
+    // }
+// });
+
+// Endpoint para obtener zonas
+app.get('/index.html/:id/agricultores', async (req, res) => {
+    // si tuviesemos filtros irian aqui? const {por caja por no se}
+    const zonasId = req.params.id;
+    try {
+        letAgriculturesPorZonas = await db.collection('agricultores').find({zonaId: new ObjectId(zonasId) }).toArray();
+        
+        //if (cajaId) {
+            // agricultoresPorZonas = agricultoresPorZonas.filter((agricultores) => agricultores.caja === cajaId);
+        // }
+
+    res.json(agricultoresPorZonas);
+    } catch (error) {
+        console.error('Error al obtener agricultores:', error);
+        res.status(500).json({error: 'hubo un problema al obtener datos'});
     }
-});
+}); 
+
+// Endpoint para obtener agricultores por zonas
+
+app.get('/index.html', async (req, res) => {
+    try {
+        console.log("conectando a mongodb");
+        if (!db) {
+            return res.status(500).json({error: 'la conexión a la base de datos no está lista'});
+        }
+    const data = await db.collection('zonas').find().toArray();
+    console.log('datos obtenidos:', data);
+    res.json(data);
+    } catch (err) {
+        console.error('Error al obtener los datos:', err);
+        res.status(500).json({error: err.message});
+    }
+}); 
+
 
 //opcionales:
 
